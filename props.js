559f08d2f3897821b9c969ad65866745bd6d2433
Satyam-Nayak <satyamn120@gmail.com>
2024-11-25 19:53:16 +0530

    1st react start again satyam nayak

---
+++ props.js
@@ -0,0 +1,3 @@
+export default function props(props){
+    return <h1>hhello {props.name}</h1>
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <header >
+        
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default App;
