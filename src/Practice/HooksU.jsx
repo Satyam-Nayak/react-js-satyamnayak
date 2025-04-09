84ac1997829564699562b61b5278a4a5ef731aa4
Satyam-Nayak <satyamn120@gmail.com>
2025-04-09 21:20:58 +0530

    react practice c

--- src/Practice/HooksU.jsx
+++ src/Practice/HooksU.jsx
@@ -1,11 +1,22 @@
-import React,{useState} from "react";
-function HooksU(){
-    const [count, setCount] = useState(0);
-    return(
-        <div>
-            <p>Counter: {count}</p>
-            <button  onClick={()=> setCount(count+1)}>Incremant</button>
-        </div>
-    )
+import React, { useState } from "react";
+
+function HooksU() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    console.log("Before setCount:", count);
+    setCount(count + 1);
+    console.log("After setCount:", count);
+    setCount(count + 1);
+    console.log("After setCount1:", count);
+  };
+
+  return (
+    <div>
+      <p>Counter: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
 }
+
 export default HooksU;
--- src/Practice/HooksU2.jsx
+++ src/Practice/HooksU2.jsx
@@ -5,6 +5,7 @@ export default function HooksU2() {
     const [count, setCount]= useState(0);
   return (
     <div>
+      
       <p>Count:</p>
       <button>Add</button>
     </div>
